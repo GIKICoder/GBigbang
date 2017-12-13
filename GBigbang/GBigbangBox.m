15dfe4c841a9f1767c9c1e3d285b7f16c8227862
gongke <gongke@sogou-inc.com>
2017-12-13 12:29:35 +0800

    Fix : 分词bug .优化分词逻辑. 增加保护.

--- GBigbang/GBigbangBox.m
+++ GBigbang/GBigbangBox.m
@@ -12,43 +12,47 @@
 
 +(NSArray *)bigBang:(NSString *)string
 {
-    __block NSMutableArray * array = [NSMutableArray array];
-    [string enumerateSubstringsInRange:NSMakeRange(0, string.length) options:NSStringEnumerationByWords usingBlock:^(NSString *subString, NSRange subStringRange, NSRange enclosingRange, BOOL *stop){
-        
-        if (subStringRange.location > enclosingRange.location) {
-            NSRange preRange =  NSMakeRange(enclosingRange.location, subStringRange.location);
-            NSString * preString = [string substringWithRange:preRange];
-            [preString enumerateSubstringsInRange:NSMakeRange(0, preString.length) options:NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString * _Nullable substring1, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {
-                [array addObject:[GBigbangItem bigbangText:substring1 isSymbol:YES]];
-            }];
-        }
-        
-        if (![GBigbangBox isEmptAllSapce:subString]) {
-            [array addObject:[GBigbangItem bigbangText:subString isSymbol:NO]];
-        }
- 
-        if (subStringRange.location > enclosingRange.location && (subStringRange.location + subStringRange.length) < (enclosingRange.location +enclosingRange.length)) {
+    @try {
+        __block NSMutableArray * array = [NSMutableArray array];
+        [string enumerateSubstringsInRange:NSMakeRange(0, string.length) options:NSStringEnumerationByWords usingBlock:^(NSString *subString, NSRange subStringRange, NSRange enclosingRange, BOOL *stop){
             
-            NSRange nextRange =  NSMakeRange(subStringRange.location + subStringRange.length, (enclosingRange.location +enclosingRange.length)-(subStringRange.location + subStringRange.length));
-            NSString * nextString = [string substringWithRange:nextRange];
-            [nextString enumerateSubstringsInRange:NSMakeRange(0, nextString.length) options:NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString * _Nullable substring2, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {
-                [array addObject:[GBigbangItem bigbangText:substring2 isSymbol:YES]];
-            }];
-        }
-        
-        if (subStringRange.length<enclosingRange.length && subStringRange.location == enclosingRange.location) {
-            NSRange subRan = NSMakeRange(subStringRange.location+subStringRange.length, enclosingRange.length-subStringRange.length);
-            if (string.length < subRan.location+subRan.length) {
-                NSString *subStr = [string substringWithRange:subRan];
-                if (subStr.length > 0) {
-                    [subStr enumerateSubstringsInRange:NSMakeRange(0, subStr.length) options:NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString * _Nullable substring3, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {
-                        [array addObject:[GBigbangItem bigbangText:substring3 isSymbol:YES]];
-                    }];
+            if (subStringRange.location > enclosingRange.location) {
+                NSRange preRange =  NSMakeRange(enclosingRange.location, subStringRange.location);
+                NSString * preString = [string substringWithRange:preRange];
+                [preString enumerateSubstringsInRange:NSMakeRange(0, preString.length) options:NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString * _Nullable substring1, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {
+                    [array addObject:[GBigbangItem bigbangText:substring1 isSymbol:YES]];
+                }];
+            }
+            
+            if (![GBigbangBox isEmptAllSapce:subString]) {
+                [array addObject:[GBigbangItem bigbangText:subString isSymbol:NO]];
+            }
+            
+            if (subStringRange.location > enclosingRange.location && (subStringRange.location + subStringRange.length) < (enclosingRange.location +enclosingRange.length)) {
+                
+                NSRange nextRange =  NSMakeRange(subStringRange.location + subStringRange.length, (enclosingRange.location +enclosingRange.length)-(subStringRange.location + subStringRange.length));
+                NSString * nextString = [string substringWithRange:nextRange];
+                [nextString enumerateSubstringsInRange:NSMakeRange(0, nextString.length) options:NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString * _Nullable substring2, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {
+                    [array addObject:[GBigbangItem bigbangText:substring2 isSymbol:YES]];
+                }];
+            }
+            
+            if (subStringRange.length<enclosingRange.length && subStringRange.location == enclosingRange.location) {
+                NSRange subRan = NSMakeRange(subStringRange.location+subStringRange.length, enclosingRange.length-subStringRange.length);
+                if (string.length > subRan.location+subRan.length) {
+                    NSString *subStr = [string substringWithRange:subRan];
+                    if (subStr.length > 0) {
+                        [subStr enumerateSubstringsInRange:NSMakeRange(0, subStr.length) options:NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString * _Nullable substring3, NSRange substringRange, NSRange enclosingRange, BOOL * _Nonnull stop) {
+                            [array addObject:[GBigbangItem bigbangText:substring3 isSymbol:YES]];
+                        }];
+                    }
                 }
             }
-        }
-    }];
-    return array.copy;
+        }];
+        return array.copy;
+    } @catch(NSException *exception)  {
+        return [NSArray array];
+    }
 }
 
 
@@ -98,3 +102,4 @@
     return item;
 }
 @end
+
